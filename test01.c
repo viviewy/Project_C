0825df5de0afa90a8ee74a2af76d8c23ee74bf30
viviewy <139833724+viviewy@users.noreply.github.com>
2023-07-24 02:15:56 +0700

    Add files via upload

---
+++ test01.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  View
+//
+//  Created by View on 18/7/2566 BE.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("viviewy");
+    return 0;
+}
